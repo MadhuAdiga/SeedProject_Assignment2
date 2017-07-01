198238fe2459807e46e169c02ae8f48d45aa0508
MadhuAdiga <madhu.vah@gmail.com>
2017-07-01 10:34:43 +0530

    Assignment2

--- app/my-new-component/my-new-component.component.ts
+++ app/my-new-component/my-new-component.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-my-new-component',
-  templateUrl: 'app/my-new-component.component.html',
-  styleUrls: ['./my-new-component.component.css']
+  selector: 'app-my-new-component',//selector tag
+  templateUrl: './my-new-component.component.html',//Url to html
+  styleUrls: ['./my-new-component.component.css']//stylesheet
 })
 export class MyNewComponentComponent implements OnInit {
 
